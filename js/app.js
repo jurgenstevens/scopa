69d3a6e91169e412b105d413e7683d2cfcfbdc11
jurgenstevens <jurgen.stevens@yahoo.com>
2022-05-20 22:23:12 -0500

    Set the comments for main game structure.

---
+++ js/app.js
@@ -0,0 +1,14 @@
+console.log("Sanity check!")
+// Constants
+
+
+// Variables
+// S1: Define variables used to keep the state of the game
+
+// Cached Elements
+
+
+// Event Listeners
+
+
+// Functionality
