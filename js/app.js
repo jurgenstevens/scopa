59767e402368b5b9819b3f903d0144247e640139
jurgenstevens <jurgen.stevens@yahoo.com>
2022-05-21 00:46:21 -0500

    Set up essential variables to hold game's state.

--- js/app.js
+++ js/app.js
@@ -4,6 +4,26 @@ console.log("Sanity check!")
 
 // Variables
 // S1: Define variables used to keep the state of the game
+let turn
+let playerHand = []
+let opponentHand = []
+let dealerDeck = []
+let tableCards = []
+let playerScore = {
+    cards: 0,
+    goldenCards: 0,
+    primiera: 0,
+    sevenOfCoins: 0,
+    scopa: 0
+}
+let opponentScore = {
+    cards: 0,
+    goldenCards: 0,
+    primiera: 0,
+    sevenOfCoins: 0,
+    scopa: 0
+}
+
 
 // Cached Elements
 
