90a068751643b14401db40d7f1502ebb123e4030
jurgenstevens <jurgen.stevens@yahoo.com>
2022-05-22 02:41:36 -0500

    Remove console logs and change querySelectors to getElementByIds.

--- js/app.js
+++ js/app.js
@@ -28,16 +28,16 @@ let opponentScore = {
 
 // Cached Elements
 // S3: Set up your cached elements to connect your JS to your HTML to get some functionality going.
-// let turnEl = document.querySelector("#turn")
-let playerScoreEl = document.querySelector("#playerScore")
-let opponentScoreEl = document.querySelector("#opponentScore")
-let messageEl = document.querySelector("#message")
-let opponentHandEl = document.querySelector("#opponentHand")
-let tableCardsEl = document.querySelector("#tableCards")
-let dealerDeckEl = document.querySelector("#dealerDeck")
-let playerHandEl = document.querySelector("#playerHand")
-let startBtnEl = document.querySelector("#startBtn")
-let resetBtnEl = document.querySelector("#resetBtn")
+// let turnEl = document.getElementById("turn")
+let playerScoreEl = document.getElementById("playerScore")
+let opponentScoreEl = document.getElementById("opponentScore")
+let messageEl = document.getElementById("message")
+let opponentHandEl = document.getElementById("opponentHand")
+let tableCardsEl = document.getElementById("tableCards")
+let dealerDeckEl = document.getElementById("dealerDeck")
+let playerHandEl = document.getElementById("playerHand")
+let startBtnEl = document.getElementById("startBtn")
+let resetBtnEl = document.getElementById("resetBtn")
 
 // Event Listeners
 // S4 Set up event listeners
@@ -50,15 +50,12 @@ startBtnEl.addEventListener("click", init
 // Functionality
 // S4.2 Create the init function to initialize the game.
 function init(){
-    console.log("Testing from the init() function!")
     // S5: When initializing the game we want to...
     // S5.1 Set round count to 0
     round = 0
-    console.log("This is round after click: ", round)
     // S5.2 Render a message to the page on the game starting and whose turn it is
     // S5.3 Push the cards to the dealer's card deck
     dealerDeck = ["clubsAce1", "clubs2", "clubs3", "clubs4", "clubs5", "clubs6", "clubs7", "clubsFante8", "clubsCavallo9", "clubsRe10", "coinsAce1", "coins2", "coins3", "coins4", "coins5", "coins6", "coins7", "coins8", "coinsFante8", "coinsCavallo9", "coinsRe10", "cupsAce1", "cups2", "cups3", "cups4", "cups5", "cups6", "cups7", "cupsFante8", "cupsCavalo9", "cupsRe10", "spadesAce1", "spades2", "spades3", "spades4", "spade5", "spades6", "spades7", "spadesFante8", "spadesCavallo9", "spadesRe10"]
-    console.log("This is dealer deck after click: ", dealerDeck)
     // S5.4 Call function to give player cards
     // S5.5 Call function to give opponent cards
 }
