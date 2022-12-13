14fde19d4163322711a52f77bb77b5455fdf48f1
jurgenstevens <jurgen.stevens@yahoo.com>
2022-12-12 21:27:12 -0600

    Create function to render field cards.

--- js/app.js
+++ js/app.js
@@ -3,8 +3,10 @@ import Deck from "./deck.js"
 
 /*-------------------------------- Constants --------------------------------*/
 // S4 - Set up the necessary variables
+// S4a: Set up the visibile variables like deck, field, turn, round, winner
 let deck, field, turn, round, winner
 
+// S4a: Set up the player with its necessary properties
 let player = {
     playerName: "Player",
     hand: [],
@@ -18,6 +20,7 @@ let player = {
     scoreTotal: 0
 }
 
+// S4b: Set up the computer with its necessary properties
 let computer = {
     playerName: "Player",
     hand: [],
@@ -37,17 +40,19 @@ let computer = {
 
 /*------------------------ Cached Element References ------------------------*/
 let startBtnEl = document.getElementById("startBtn")
-
-
+let roundEl = document.getElementById("round")
+let fieldCardsEl = document.getElementById("fieldCards")
+let fieldEl = document.querySelector(".field")
+let dealerDeckEl = document.getElementById("dealerDeck")
+let playerHandEl = document.getElementById("playerHand")
 
 /*----------------------------- Event Listeners -----------------------------*/
 startBtnEl.addEventListener("click", init)
 
-
 /*-------------------------------- Functions --------------------------------*/
 
 
-// S4 - Create a new deck and shuffle it by initializing the game and setting the turn and round to 1
+// S7 - Create a new deck and shuffle it by initializing the game and setting the turn and round to 1
 function init(){
     turn = 1
     round = 1
@@ -55,14 +60,30 @@ function init(){
     deck = new Deck()
     deck.reset()
     deck.shuffle()
-    console.log("Deck before deal: ", deck)
     field = deck.dealField()
     player.hand = deck.dealPlayer()
     computer.hand = deck.dealComputer()
-    console.log("Deck before deal: ", deck)
-    console.log("Field cards: ", field)
-    console.log("Player's Hand: ", player.hand)
-    console.log("Computer's Hand: ", computer.hand)
+    // S7a: create a ul to store cards in field
+    const fieldCardsUl = document.createElement('ul')
+    render()
 }
 
-// S7: Create functions to render player, computer and field hands ref Ian's function on lines 488 - 512
+// S8: Create functions to render player, computer and field hands ref Ian's function on lines 488 - 512
+function render(){
+    roundEl.innerText = `Round: ${round}`
+    dealerDeckEl.innerText = `${deck.deck.length} cards left`
+    renderFieldCards()
+}
+
+// S8a: Create a function to render the four field cards
+function renderFieldCards(){
+    fieldEl.innerHTML = ""
+    field.forEach((card) => {
+        let fieldCard = document.createElement('div')
+        fieldCard.className = 'field-card'
+        fieldCard.textContent = `${card}`
+        fieldEl.appendChild(fieldCard)
+    })
+}
+// S9: Render the three (upside down) opponent cards
+// S10: Render the three player cards
